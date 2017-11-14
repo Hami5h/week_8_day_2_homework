3106f370666925580032ac861b2f19963482e812
Hamish <hamishfpso@gmail.com>
2017-11-14 16:45:52 +0000

    menu directory created and infaltor

--- app/src/main/java/com/example/hamishstewart/wordcounter/Controller/MainActivity.java
+++ app/src/main/java/com/example/hamishstewart/wordcounter/Controller/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,4 +42,13 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+
 }
