81ef5db6d8e67bab7a7db8e417e0cde4a1dc7d98
Hamish <hamishfpso@gmail.com>
2017-11-14 17:28:07 +0000

    About text added

--- app/src/main/java/com/example/hamishstewart/wordcounter/Controller/AboutActivity.java
+++ app/src/main/java/com/example/hamishstewart/wordcounter/Controller/AboutActivity.java
@@ -2,14 +2,20 @@ package com.example.hamishstewart.wordcounter.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.hamishstewart.wordcounter.R;
 
 public class AboutActivity extends AppCompatActivity {
+    private TextView aboutMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        this.aboutMe = (TextView) findViewById(R.id.aboutApp);
+
+        this.aboutMe.setText("Hi I am an Android App developed by Hamish");
     }
 }
