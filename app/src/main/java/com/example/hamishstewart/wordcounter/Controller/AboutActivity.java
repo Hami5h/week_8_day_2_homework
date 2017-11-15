1d4f80f5166a0712f4c07de0f2c9791aaec8b9c6
Hamish <hamishfpso@gmail.com>
2017-11-15 09:11:18 +0000

    Some adjustments

--- app/src/main/java/com/example/hamishstewart/wordcounter/Controller/AboutActivity.java
+++ app/src/main/java/com/example/hamishstewart/wordcounter/Controller/AboutActivity.java
@@ -7,15 +7,11 @@ import android.widget.TextView;
 import com.example.hamishstewart.wordcounter.R;
 
 public class AboutActivity extends AppCompatActivity {
-    private TextView aboutMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        this.aboutMe = (TextView) findViewById(R.id.aboutApp);
-
-        this.aboutMe.setText("Hi I am an Android App developed by Hamish");
     }
 }
